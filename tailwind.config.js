249b705abbe388b6ccd598a6ced04a1304a2fbb5
Ross <528266131@qq.com>
2025-02-27 12:36:21 -0500

    style: remove unused pictures and add og

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ export default {
     extend: {
       colors: {
         "exhibition-green": "#2C3A2F",
+        "config-1": "#B7C4CF",
+        "config-2":"#EEE3CB",
+        "config-3":"#D7C0AE",
+        "config-4":"#967E76",
       },
       fontFamily: {
         serif: ["Cormorant Garamond", "serif"],
