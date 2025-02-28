0a15e38f28335b9ff6669b496b78700ef4c28f15
Ross <528266131@qq.com>
2025-02-27 23:06:08 -0500

    dev: change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://jimmieluo.com",
+  site: "https://www.jimmieluo.com",
   base: "",
   output: "static",
 });
